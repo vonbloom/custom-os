258bedf69b30a7c421f9bfcb0d2e1b04865d726f
Roger Alasà Duch <vonbloom@gmail.com>
2025-12-18 18:09:35 +0100

    Add language config. Clean dnf cache

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,9 @@ echo 'roger ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/roger
 mkdir -p /usr/lib/bootc/kargs.d
 echo 'kargs = ["quiet", "rhgb", "splash"]' > /usr/lib/bootc/kargs.d/00-quiet.toml
 
+### Set language
+echo "LANG=ca_ES.UTF-8" > /etc/locale.conf
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -20,7 +23,8 @@ echo 'kargs = ["quiet", "rhgb", "splash"]' > /usr/lib/bootc/kargs.d/00-quiet.tom
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y NetworkManager-wifi iwlwifi-mvm-firmware linux-firmware
+dnf5 install -y NetworkManager-wifi iwlwifi-mvm-firmware linux-firmware glibc-langpack-ca
+dnf5 clean all
 
 # Use a COPR Example:
 #
