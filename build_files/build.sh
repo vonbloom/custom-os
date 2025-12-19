3b7e8cdf6214c36d5dcfc7fed88de00ef8778723
Roger Alasà Duch <vonbloom@gmail.com>
2025-12-19 12:33:36 +0100

    Uninstall qemu stuff. Fix nix install path. Fix build disk github workflow

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 ### Set users
 useradd -m -G wheel roger
-echo "root:\$6\$MKg6cGNyAXu4pqcM\$DOVdXJ4SIPeDtsaMf8DK3mvJzuvUCALLESWLWbdTwKLq58tY2mg8sRR9W1k3yLohj69afFtc/jtGG9qolpddF." | chpasswd -e
+echo "root:\$6\$HBBMFHo9PCNKpqjr\$scCKm1BSP60VQKSHHTHcUICdCTcY534/C09KMjNehFXhgz8OXDgVbp7iTkuLlwAqQKJ8jIKXcD07HGqs4gqRJ/" | chpasswd -e
 echo "roger:\$6\$j80GfNM2z91f61qz\$Rn3VOL2KPPXZZNDgtvO0SQWdmArC3cmO0kSiw9MaT4TirmFr1so.GzIPz2BYbg86ob2tqXxNqWXGYZrbE7tu90" | chpasswd -e
 echo 'roger ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/roger
 
@@ -24,7 +24,8 @@ echo "LANG=ca_ES.UTF-8" > /etc/locale.conf
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this removes packages
-dnf5 remove -y nano nano-default-editor
+dnf5 remove -y nano nano-default-editor "qemu-user-static*"
+rm -f /usr/lib/binfmt.d/qemu-*
 
 # this installs package from fedora repos
 dnf5 install -y \
@@ -46,8 +47,11 @@ dnf5 clean all
 # dnf5 -y copr disable ublue-os/staging
 
 ### Set nix path
-rm -rf /var/nix
-mv /nix /var/nix
+mkdir -p /var/nix
+if [ -d /nix ]; then
+    cp -a /nix/. /var/nix/ || true
+    rm -rf /nix
+fi
 ln -s /var/nix /nix
 
 #### Example for enabling a System Unit File
